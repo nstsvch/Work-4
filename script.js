9a1532e54b82e795293d258fcac493880f8989cd
Anastasiya <a5rs5a8r8gmail.com>
2021-07-06 13:24:09 +0300

    Third

--- script.js
+++ script.js
@@ -14,5 +14,37 @@ window.addEventListener('load', function() {
             ar.style.transform = 'rotateX(180deg)';
             btn.classList.add('show-btn');
         }
-    } 
+    }
 });
+
+function sort(ascending, columnClassName) {
+    var tbody = document.getElementById("season-table").getElementsByTagName("tbody")[0];
+    var rows = tbody.getElementsByTagName("tr");
+
+    var unsorted = true;
+
+    while (unsorted) {
+        unsorted = false
+
+        for (var r = 0; r < rows.length - 1; r++) {
+            var row = rows[r];
+            var nextRow = rows[r + 1];
+
+            var value = row.getElementsByClassName(columnClassName)[0].innerHTML;
+            var nextValue = nextRow.getElementsByClassName(columnClassName)[0].innerHTML;
+
+            value = value.replace(',', '.'); // in case a comma is used in float number
+            nextValue = nextValue.replace(',', '.');
+
+            if (!isNaN(value)) {
+                value = parseFloat(value);
+                nextValue = parseFloat(nextValue);
+            }
+
+            if (ascending ? value < nextValue : value > nextValue) {
+                tbody.insertBefore(nextRow, row);
+                unsorted = true;
+            }
+        }
+    }
+};
